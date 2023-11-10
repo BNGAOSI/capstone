d95a4b9b0f801b819fbabcdc999f25808f0ad414
BNGAOSI <ben9897@students.uc-bcf.edu.ph>
2023-11-10 16:01:01 +0800

    Dashboard screen modified

--- dashboardEdEddnEddyBarangayApp/app/src/main/java/com/example/dashboard_ededdneddy_barangayapp/home_activity.java
+++ dashboardEdEddnEddyBarangayApp/app/src/main/java/com/example/dashboard_ededdneddy_barangayapp/home_activity.java
@@ -1,16 +1,18 @@
 package com.example.dashboard_ededdneddy_barangayapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class home_activity extends Activity {
 
-	
+	private Button button;
 	private View _bg__home_ek2;
 	private View _bg__android_status_bar_ek1;
 	private View _bg__icons_ek1;
@@ -25,7 +27,6 @@ public class home_activity extends Activity {
 	private View _bg__title_ek1;
 	private TextView welcome__user_;
 	private View _bg__donate_ek1;
-	private View button;
 	private View button_ek1;
 	private TextView appointment;
 	private View _bg__sell_ek1;
@@ -47,13 +48,12 @@ public class home_activity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
 
-		
+
 		_bg__home_ek2 = (View) findViewById(R.id._bg__home_ek2);
 		profile_icon_1 = (ImageView) findViewById(R.id.profile_icon_1);
 		_bg__title_ek1 = (View) findViewById(R.id._bg__title_ek1);
 		welcome__user_ = (TextView) findViewById(R.id.welcome__user_);
 		_bg__donate_ek1 = (View) findViewById(R.id._bg__donate_ek1);
-		button = (View) findViewById(R.id.button);
 		button_ek1 = (View) findViewById(R.id.button_ek1);
 		appointment = (TextView) findViewById(R.id.appointment);
 		_bg__sell_ek1 = (View) findViewById(R.id._bg__sell_ek1);
@@ -71,7 +71,18 @@ public class home_activity extends Activity {
 	
 		
 		//custom code goes here
-	
+		button = (Button) findViewById(R.id.appointmentButton);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				openactivity_appointment();
+			}
+		});
+	}
+	public void openactivity_appointment(){
+		Intent intent;
+		intent = new Intent(this, activity_appointment.class);
+		startActivity(intent);
 	}
 }
 	
